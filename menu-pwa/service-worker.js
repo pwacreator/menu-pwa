de65846c82b24235224f1dffac084d6eb4b9a7ff
pwacreator <gabriel.barben@stud.gymthun.ch>
2026-01-30 17:26:29 +0100

    Add files via upload

---
+++ menu-pwa/service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "menu-cache-v1";
+const FILES_TO_CACHE = [
+  "index.html",
+  "style.css",
+  "script.js",
+  "manifest.json",
+  "assets/images/pizza.jpg",
+  "assets/images/burger.jpg",
+  "assets/images/pasta.jpg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
